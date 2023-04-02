28128d3402279f0ad6e53266e2d9274bb6c9c9e9
gokuUii <129477579+gokuUii@users.noreply.github.com>
2023-04-02 19:13:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/etypmBc.jpeg' : process.env.ALIVE_LOGO,
+FOOTER: process.env.FOOTER === undefined ? 'https://i.imgur.com/etypmBc.jpeg' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*https://i.imgur.com/etypmBc.jpeg*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID:'ASTRO=hv4y1C6Z#fUYEcANWtiGvrLggznfp_OClbCCzEdkDhVM2LyD4PyE'
 };
